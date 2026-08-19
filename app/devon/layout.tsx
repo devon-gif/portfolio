bd7db1bddcd5b85896d7480997b9fe671e1b5beb
devon-gif <heydevon@gmail.com>
2026-08-19 08:22:10 -0600

    Load refined Devon portfolio styles

--- app/devon/layout.tsx
+++ app/devon/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./devon.css";
+import "./devon-v2.css";
 
 export default function DevonLayout({ children }: { children: ReactNode }) {
   return <>{children}</>;
