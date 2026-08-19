0ff60fa3077b3805c73923c5b5c57e048e52ee8d
devon-gif <heydevon@gmail.com>
2026-08-19 09:17:02 -0600

    Mount Auto Creative OS showcase on Devon portfolio

--- app/devon/layout.tsx
+++ app/devon/layout.tsx
@@ -1,7 +1,13 @@
 import type { ReactNode } from "react";
+import { AutoCreativeMainPageShowcase } from "./components/AutoCreativeMainPageShowcase";
 import "./devon.css";
 import "./devon-v2.css";
 
 export default function DevonLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      <AutoCreativeMainPageShowcase />
+    </>
+  );
 }
