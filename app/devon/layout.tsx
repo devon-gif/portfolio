43be66811ffca2e1ad922d672d787b539bb1cf82
devon-gif <heydevon@gmail.com>
2026-09-09 11:51:19 -0600

    Render featured project builds on Devon portfolio

--- app/devon/layout.tsx
+++ app/devon/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import { AutoCreativeMainPageShowcase } from "./components/AutoCreativeMainPageShowcase";
+import { FeaturedBuildsShowcase } from "./components/FeaturedBuildsShowcase";
 import "./devon.css";
 import "./devon-v2.css";
 
@@ -7,6 +8,7 @@ export default function DevonLayout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
+      <FeaturedBuildsShowcase />
       <AutoCreativeMainPageShowcase />
     </>
   );
