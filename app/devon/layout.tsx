dd07a234401a61af1f462d5263cd74259da322cb
devon-gif <heydevon@gmail.com>
2026-09-09 17:01:02 -0600

    Use Realiz portfolio shell for Devon route

--- app/devon/layout.tsx
+++ app/devon/layout.tsx
@@ -1,15 +1,8 @@
 import type { ReactNode } from "react";
-import { AutoCreativeMainPageShowcase } from "./components/AutoCreativeMainPageShowcase";
-import { FeaturedBuildsShowcase } from "./components/FeaturedBuildsShowcase";
 import "./devon.css";
 import "./devon-v2.css";
+import "./realiz.css";
 
 export default function DevonLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      {children}
-      <FeaturedBuildsShowcase />
-      <AutoCreativeMainPageShowcase />
-    </>
-  );
+  return <>{children}</>;
 }
