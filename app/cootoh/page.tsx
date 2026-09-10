04aec6796ffcc8ccfcde52a05cfc808fbadcb767
devon-gif <heydevon@gmail.com>
2026-09-10 15:50:31 -0600

    Add Cootoh landing page entry

---
+++ app/cootoh/page.tsx
@@ -0,0 +1,10 @@
+import { fraunces } from "@/components/marketing/studioFont";
+import { CootohLanding } from "./CootohLanding";
+
+export default function CootohPage() {
+  return (
+    <main className={fraunces.variable}>
+      <CootohLanding />
+    </main>
+  );
+}
