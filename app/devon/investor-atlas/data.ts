598e0562009f7d290ae5c2aa38d1a64eeabf73dd
devon-gif <heydevon@gmail.com>
2026-09-24 11:27:30 -0600

    Assemble investor image atlas data

---
+++ app/devon/investor-atlas/data.ts
@@ -0,0 +1,11 @@
+import c0 from "./chunk0";
+import c1 from "./chunk1";
+import c2 from "./chunk2";
+import c3 from "./chunk3";
+import c4 from "./chunk4";
+import c5 from "./chunk5";
+import c6 from "./chunk6";
+import c7 from "./chunk7";
+
+export const INVESTOR_ATLAS_DATA_URI =
+  "data:image/webp;base64," + c0 + c1 + c2 + c3 + c4 + c5 + c6 + c7;
